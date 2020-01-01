67ba4a72684bb48d1bb8e3a8941e79b56af6cbf0
Firuz Afzalov <afzalov98@gmail.com>
2020-01-01 04:09:39 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,6 +1,15 @@
 def monopoly
-  monopoly = {
+    monopoly = {
     railroads: {}
   }
 end
 
+def monopoly_with_second_tier
+    monopoly = {
+    railroads: {
+      pieces: 4,
+      names: {},
+      rent_in_dollars: {}
+    }
+  }
+end
